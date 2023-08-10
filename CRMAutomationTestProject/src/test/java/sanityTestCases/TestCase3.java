2599512ea36856e251f991f60018d7a4a6da0105
DESKTOP-OHDR9CR\Shivam Gulati <gulati.shivam2008@gmail.com>
2023-08-10 11:00:06 +0530

    I have created test case3

---
+++ CRMAutomationTestProject/src/test/java/sanityTestCases/TestCase3.java
@@ -0,0 +1,13 @@
+package sanityTestCases;
+
+import org.testng.annotations.Test;
+
+public class TestCase3 {
+
+
+    @Test
+    public void TestCase3(){
+
+        System.out.println("This is test case 3 by Priya");
+    }
+}
