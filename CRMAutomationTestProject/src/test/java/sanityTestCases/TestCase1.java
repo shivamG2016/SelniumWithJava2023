96e871f85a352825fc9f7adecf98a9d2cc2e491f
DESKTOP-OHDR9CR\Shivam Gulati <gulati.shivam2008@gmail.com>
2023-08-10 10:45:31 +0530

    My First Project initial commit

---
+++ CRMAutomationTestProject/src/test/java/sanityTestCases/TestCase1.java
@@ -0,0 +1,14 @@
+package sanityTestCases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+
+    @Test
+    public void TestCase1(){
+
+        System.out.println("First Test Case 1");
+
+    }
+}
